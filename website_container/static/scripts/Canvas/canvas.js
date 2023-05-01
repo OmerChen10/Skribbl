1d896ec272e7c8d1e2169fcc0b5798e47f76958d
OmerChen10 <o.c33211@gmail.com>
2023-05-01 23:58:22 +0300

    Added a canvas class

--- website_container/static/scripts/Canvas/canvas.js
+++ website_container/static/scripts/Canvas/canvas.js
@@ -1,74 +1,82 @@
 
-var canvas, ctx, canvasPos;
-function initializeGame() {
-    // This function is used to initialize the game canvas.
-    var gameElements = document.querySelector('.game');
-    gameElements.style.display = 'flex';
+class Canvas {
+    constructor(){
+        this.canvas = document.querySelector('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        
+        this.canvas.width = this.canvas.offsetWidth;
+        this.canvas.height = this.canvas.offsetHeight;
 
-    canvas = document.querySelector('canvas');
-    ctx = canvas.getContext('2d');
-    
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
+        this.canvasPos = this.canvas.getBoundingClientRect();
+    }
 
-    canvasPos = canvas.getBoundingClientRect();
+    enableDrawing(){
+        console.log('DRAWING ENABLED'); 
 
-    console.log('GAME INITIALIZED');
-    draw();
+        this.isDrawing = false;
+
+        const drawing = (e) => {
+            if (!this.isDrawing) return;
+
+            this.ctx.lineTo(e.offsetX, e.offsetY);
+            this.ctx.stroke();
+        }
+
+        const startDrawing = (e) => {
+            this.ctx.beginPath();
+            this.isDrawing = true;
+        }
+
+        const stopDrawing = (e) => {
+            this.ctx.closePath();
+            this.isDrawing = false;
+        }
+
+        // Support for mobile devices
+
+        const touchStartDrawing = (e) => {
+            this.ctx.beginPath();
+            this.isDrawing = true;
+        }
+
+        const touchStopDrawing = (e) => {
+            this.ctx.closePath();
+            this.isDrawing = false;
+        }
+
+        const touchDrawing = (e) => {
+            if (!this.isDrawing) return;
+
+            // Calculate the position of the touch relative to the canvas
+            offsetX = e.touches[0].clientX - this.canvasPos.left;
+            offsetY = e.touches[0].clientY - this.canvasPos.top;
+
+            this.ctx.lineTo(offsetX, offsetY);
+            this.ctx.stroke();
+        }
+
+        // Event listeners for mouse events
+        this.canvas.addEventListener("mousedown", startDrawing);
+        this.canvas.addEventListener("mouseup", stopDrawing);
+        this.canvas.addEventListener("mousemove", drawing);
+
+        // Event listeners for touch events
+        this.canvas.addEventListener("touchstart", touchStartDrawing);
+        this.canvas.addEventListener("touchend", touchStopDrawing);
+        this.canvas.addEventListener("touchmove", touchDrawing);
+    }
+
+    disableDrawing(){
+        console.log('DRAWING DISABLED'); 
+
+        // Remove all event listeners
+        this.canvas.removeEventListener("mousedown", startDrawing);
+        this.canvas.removeEventListener("mouseup", stopDrawing);
+        this.canvas.removeEventListener("mousemove", drawing);
+
+        this.canvas.removeEventListener("touchstart", touchStartDrawing);
+        this.canvas.removeEventListener("touchend", touchStopDrawing);
+        this.canvas.removeEventListener("touchmove", touchDrawing);
+    }
 }
 
-function draw() {
-    console.log('DRAWING ENABLED'); 
-
-    isDrawing = false;
-
-    const drawing = (e) => {
-        if (!isDrawing) return;
-
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
-    }
-
-    const startDrawing = (e) => {
-        ctx.beginPath();
-        isDrawing = true;
-    }
-
-    const stopDrawing = (e) => {
-        ctx.closePath();
-        isDrawing = false;
-    }
-
-    // Support for mobile devices
-
-    const touchStartDrawing = (e) => {
-        ctx.beginPath();
-        isDrawing = true;
-    }
-
-    const touchStopDrawing = (e) => {
-        ctx.closePath();
-        isDrawing = false;
-    }
-
-    const touchDrawing = (e) => {
-        if (!isDrawing) return;
-
-        // Calculate the position of the touch relative to the canvas
-        offsetX = e.touches[0].clientX - canvasPos.left;
-        offsetY = e.touches[0].clientY - canvasPos.top;
-
-        ctx.lineTo(offsetX, offsetY);
-        ctx.stroke();
-    }
-
-    // Event listeners for mouse events
-    canvas.addEventListener("mousedown", startDrawing);
-    canvas.addEventListener("mouseup", stopDrawing);
-    canvas.addEventListener("mousemove", drawing);
-
-    // Event listeners for touch events
-    canvas.addEventListener("touchstart", touchStartDrawing);
-    canvas.addEventListener("touchend", touchStopDrawing);
-    canvas.addEventListener("touchmove", touchDrawing);
-};
