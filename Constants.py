80db74219c7101a5a3c8cec35c22bca49d39ee87
OmerChen10 <o.c33211@gmail.com>
2023-05-17 21:45:27 +0300

    Constants are now in caps (super important)

--- Constants.py
+++ Constants.py
@@ -3,20 +3,20 @@ import os
 
 class GameConfig():
 
-    code_length = 4
-    round_duration = 30 # Seconds
+    CODE_LENGTH = 4
+    ROUND_DURATION = 30 # Seconds
 
     # Words 
-    starting_percentage = 0.25 # The percentage of the word that is shown at the start of the round.
-    reveal_interval = round_duration * 0.6 # In round time percentage.
-    max_reveal_percentage = 0.35 # The maximum percentage of the word that can be revealed.
+    STARTING_PERCENTAGE = 0.25 # The percentage of the word that is shown at the start of the round.
+    REVEAL_INTERVAL = ROUND_DURATION * 0.6 # The interval at which the letter is revealed.
+    MAX_REVEAL_PERCENTAGE = 0.35 # The maximum percentage of the word that can be revealed.
 
-    words_file_dir = os.path.join(os.path.dirname(__file__), "words.txt")
+    WORDS_FILE_DIR = os.path.join(os.path.dirname(__file__), "words.txt") # The directory of the words file.
 
 
 class NetworkConfig():
-    website_port = 80
-    reconnection_enabled = False
+    WEBSITE_PORT = 80
+    RECONNECTION_ENABLED = False
 
 
 class Headers():
@@ -25,4 +25,7 @@ class Headers():
     PLAYER_ROLE = 3
     CANVAS_UPDATE = 4
     WORD_UPDATE = 5
+    GUESS = 6
+    GUESS_CORRECT = 7
+
     
